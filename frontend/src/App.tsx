83d50b592bc569baa101ff8775e41296b96a4d5e
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-07-30 01:11:02 -0300

    fix: delete unused contexts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,5 @@
 import { ThemeProvider } from 'styled-components'
 import { BrowserRouter } from 'react-router-dom'
-import { GithubUserProvider } from './contexts/GithubUserContext'
-import { IssuesProvider } from './contexts/IssuesContext'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { Router } from './Router'
@@ -14,11 +12,7 @@ export function App() {
 
       <BrowserRouter>
         <InvoicesProvider>
-          <GithubUserProvider>
-            <IssuesProvider>
-              <Router />
-            </IssuesProvider>
-          </GithubUserProvider>
+          <Router />
         </InvoicesProvider>
       </BrowserRouter>
     </ThemeProvider>
