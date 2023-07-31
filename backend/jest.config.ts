aee51b6ee0ef31e571906eed5565951b5aa2909a
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-07-31 11:57:15 -0300

    chore: add test commands and configs

---
+++ backend/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  testMatch: ['**/*.spec.ts'],
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
+}
+
+export default config
