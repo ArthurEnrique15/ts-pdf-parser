760f92c667e205acf5d991a1b8f67be90b7c7154
ArthurEnrique15 <arthurenrique1512@gmail.com>
2023-07-31 18:05:04 -0300

    feat: add express async errors to keep server running if something throws

--- backend/src/main/app.ts
+++ backend/src/main/app.ts
@@ -1,5 +1,6 @@
-import express from 'express'
+import express, { NextFunction, Response, Request } from 'express'
 import cors from 'cors'
+import 'express-async-errors'
 
 import { router } from './routes'
 
@@ -9,5 +10,13 @@ app.use(cors())
 app.use(express.json())
 
 app.use(router)
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  console.error(err)
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  })
+})
 
 export { app }
